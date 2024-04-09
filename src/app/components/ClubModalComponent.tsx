9fb2c3e790f058b0a62d7db05c08281178a38954
sinnchinn <sinathachin@gmail.com>
2024-04-09 15:32:12 -0700

    starting club logic

--- src/app/components/ClubModalComponent.tsx
+++ src/app/components/ClubModalComponent.tsx
@@ -1,18 +1,75 @@
 "use client";
 
-import { Button, Dropdown, Modal } from "flowbite-react";
+import { Button, Dropdown, FileInput, Modal } from "flowbite-react";
 import { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
+import { IClubs } from "@/Interfaces/Interfaces";
+import { getClubItemsByLeaderId, loggedInData, updateClubs } from "@/utils/DataServices";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 function ClubModalComponent() {
     const [openModal, setOpenModal] = useState(false);
+    const [clubItems, setClubItems] = useState<IClubs[]>();
+
+    const [clubName, setClubName] = useState<string>("");
+    const [clubDescription, setClubDescription] = useState<string>("");
+    const [clubImg, setClubImg] = useState<any>("");
     const [privateClub, setPrivateClub] = useState<boolean>(false);
 
-    const privateSettingOn= () => {
+    const VisuallyHiddenInput = styled('input')({
+        clip: 'rect(0 0 0 0)',
+        clipPath: 'inset(50%)',
+        height: 1,
+        overflow: 'hidden',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        whiteSpace: 'nowrap',
+        width: 1,
+    });
+
+
+    const privateSettingOn = () => {
         setPrivateClub(true)
     }
     const publicSettingOn = () => {
         setPrivateClub(false)
+    }
+
+    const handleCreateClub = async (clubs: IClubs) => {
+        let result = await updateClubs(clubs)
+
+        if (result) {
+            const loggedIn = loggedInData();
+            let userClubs: IClubs[] = await getClubItemsByLeaderId(loggedIn.leaderId)
+            let filteredClubs = userClubs.filter(club => club.isDeleted == false)
+            setClubItems(filteredClubs);
+        }
+
+    }
+
+    const handleClubName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setClubName(e.target.value);
+    }
+
+    const handleClubDescription = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setClubDescription(e.target.value);
+    }
+
+    const handleClubImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let reader = new FileReader();
+        const file = e.target.files?.[0];
+
+        if (file) {
+            reader.onload = () => {
+                setClubImg(reader.result)
+                console.log(reader.result);
+            }
+            reader.readAsDataURL(file);
+        }
+
     }
 
     return (
@@ -20,14 +77,14 @@ function ClubModalComponent() {
             <Button className="darkBlue rounded-xl enabled:hover:bg-darkerblue focus:ring-0" onClick={() => setOpenModal(true)}>
                 <span className="font-mainFont text-[22px]">Create Club</span>
                 {/* <img alt="plus sign" src={AddIcon} /> */}
-                <AddIcon className="ms-2"/>
+                <AddIcon className="ms-2" />
             </Button>
             <Modal show={openModal} onClose={() => setOpenModal(false)}>
-                
+
                 <Modal.Body className="darkBeige rounded-lg px-10">
-                        <div className="flex justify-end justify-items-end">
+                    <div className="flex justify-end justify-items-end">
                         <button className="text-xl" onClick={() => setOpenModal(false)}>X</button>
-                        </div>
+                    </div>
                     <div className="pb-5 pt-2 flex flex-1 justify-center">
                         <h1 className="text-center text-3xl font-mainFont font-bold">Create New Club</h1>
                     </div>
@@ -35,6 +92,17 @@ function ClubModalComponent() {
                         <div className="flex-col pb-3">
                             <label className="font-mainFont text-lg">Add Cover Image:</label>
                             <button className="opaqueWhite px-4 py-1 rounded-xl ms-2 font-mainFont">Select File</button>
+                            <FileInput className="bg-ivory" onChange={handleClubImg} accept='image/png. image/jpg' id="picture" placeholder='choose image' required />
+                            {/* <Button
+                                component="label"
+                                role={undefined}
+                                variant="contained"
+                                tabIndex={-1}
+                                startIcon={<CloudUploadIcon />}
+                            >
+                                Upload file
+                                <VisuallyHiddenInput type="file" />
+                            </Button> */}
                         </div>
                         <div className="py-2">
                             <label className="font-mainFont text-lg">Club Name:</label>
@@ -57,7 +125,7 @@ function ClubModalComponent() {
                                     <option value="public" className="font-mainFont" onClick={publicSettingOn}>Public</option>
                                     <option value="priv" onClick={privateSettingOn}>Private</option>
                                 </select>
-                            {/* <Dropdown label={!privateClub ? "Public" : "Private"} style={{color: "black"}} dismissOnClick={false}>
+                                {/* <Dropdown label={!privateClub ? "Public" : "Private"} style={{color: "black"}} dismissOnClick={false}>
                                 <Dropdown.Item onClick={publicSettingOn}>Public</Dropdown.Item>
                                 <Dropdown.Item onClick={privateSettingOn}>Private</Dropdown.Item>
                             </Dropdown> */}
@@ -65,14 +133,14 @@ function ClubModalComponent() {
                         </div>
                     </div>
                     <div className="flex flex-1 justify-end mt-48">
-                    <Button className="darkBlue rounded-xl" onClick={() => setOpenModal(false)}>
-                        <span className="font-mainFont text-lg">Create</span>
-                        {/* <img alt="plus sign" src=""/> */}
-                        <AddIcon className="ms-1"/>
-                    </Button>
+                        <Button className="darkBlue rounded-xl" onClick={() => setOpenModal(false)}>
+                            <span className="font-mainFont text-lg">Create</span>
+                            {/* <img alt="plus sign" src=""/> */}
+                            <AddIcon className="ms-1" />
+                        </Button>
                     </div>
                 </Modal.Body>
-               
+
             </Modal>
         </>
     );
--- src/utils/DataServices.ts
+++ src/utils/DataServices.ts
@@ -1,6 +1,7 @@
 import { IClubs, ILoginUserInfo, IToken, IUserData } from "@/Interfaces/Interfaces";
 
 const url = 'https://mangadictionapi.azurewebsites.net/';
+let userData: IClubs
 
 export const createUser = async (createdUser: IUserData) => {
 
@@ -40,6 +41,43 @@ export const login = async(loginUser: ILoginUserInfo) => {
     return data;
 }
 
+export const createClub = async (Club: IClubs) => {
+    const res = await fetch(url + 'Club/CreateClub', {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(Club)
+    });
+
+    if(!res.ok){
+        const message = 'An error has occured: ' + res.status;
+        throw new Error(message);
+    }
+
+    const data = await res.json;
+    return data;
+
+}
+
+export const updateClubs = async (Club: IClubs) => {
+    const res = await fetch(url + 'Club/UpdateClub/', {
+        method: "PUT",
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(Club)
+    });
+
+    if(!res.ok){
+        const message = 'An error has occured: ' + res.status;
+        throw new Error(message);
+    }
+
+    const data = await res.json();
+    return data;
+}
+
 export const checkToken = () => {
     let result = false;
     let isData = localStorage.getItem("Token");
@@ -51,9 +89,25 @@ export const checkToken = () => {
     return result;
 }
 
+export const getClubItemsByLeaderId = async (leaderId: number) => {
+    const res = await fetch(url + 'Club/GetClubById/' + leaderId)
+    const data = await res.json();
+    // console.log(data);
+    return data;
+}
+
+// getClubItemsByLeaderId(1);
+
+export const loggedInData = () => {
+    return userData;
+}
+
 // Get Public Clubs
 export const publicClubsApi = async() => {
     const promise = await fetch('https://mangadictionapi.azurewebsites.net/Club/GetAllClubs');
     const data: IClubs[] = await promise.json();
+    console.log(data);
     return data;
 }
+
+publicClubsApi();
