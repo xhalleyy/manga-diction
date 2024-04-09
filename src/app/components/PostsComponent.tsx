a873f429fb7ab94d45fa92fa883f61271f58d8de
Halley Pham <xhalleyy@gmail.com>
2024-04-09 01:09:50 -0700

    fixed icons

--- src/app/components/PostsComponent.tsx
+++ src/app/components/PostsComponent.tsx
@@ -1,36 +1,41 @@
+'use client'
+
 import { Avatar, Badge } from 'flowbite-react'
 import React from 'react'
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
 
 const PostsComponent = () => {
     return (
-        <div className='flex font-mainFont w-3/5'>
-            <div style={{ width: '10%' }} className='flex-col'>
+        <div className='flex font-mainFont w-3/5 bg-white/80 rounded-md'>
+            <div style={{ width: '10%' }} className='flex-col mt-1.5'>
                 <Avatar rounded />
             </div>
 
-            <div style={{ width: '90%' }} className='flex-col'>
+            <div style={{ width: '90%' }} className='flex-col mt-1.5'>
                 <p className='text-xl'> username </p>
 
                 <div className='inline-flex'>
-                    <Badge className='bg-tagsBg rounded-lg text-white mr-1'>tags</Badge>
+                    <Badge className='bg-tagsBg rounded-lg text-white px-2 mr-1'>tags</Badge>
                     <Badge className='bg-tagsBg rounded-lg text-white'>tags</Badge>
                 </div>
 
                 <div>
                     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis maximus nunc, ac rhoncus odio congue quis. Sed ac semper orci, eu lacus</p>
-                    <a> (see more) </a>
+                    <p> (see more) </p>
                 </div>
 
-                <div className='inline-flex'>
-                    <Badge className='flex flex-row border rounded-lg text-black font-normal mr-1'>
-                            <img src='/likes.png' />
-                            <span>comments</span>
-                    </Badge>
+                <div className='inline-flex gap-1 mb-2'>
+                    <div className='flex border border-black rounded-xl h-6 text-black font-normal mr-1 px-5 justify-around items-center gap-3 cursor-pointer'>
+                            <ThumbUpOutlinedIcon sx={{fontSize: '16px'}}/>
+                            <div>0</div>
+                    </div>
+                    
 
-                    <Badge className='border text-black font-normal rounded-lg '>
-                        <img src='/comments.png' />
+                    <div className='flex border border-black rounded-xl h-6 text-black font-normal mr-1 px-5 justify-around items-center gap-3 cursor-pointer'>
+                        <ModeCommentOutlinedIcon sx={{fontSize: '16px'}}/>
                         <p>likes</p>
-                    </Badge>
+                    </div>
                 </div>
 
             </div>
